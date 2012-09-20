d6adcea8b995e6910f4bd8e7e017f14df4b06f53
Shuo Han <hanshuo.cmu@gmail.com>
2012-09-19 23:21:52 -0400

    First update

--- MyCrawler/src/TestMyCrawler.java
+++ MyCrawler/src/TestMyCrawler.java
@@ -5,7 +5,7 @@ public class TestMyCrawler {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello world again!!");
 
 	}
 
